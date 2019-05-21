1320f68387d194a5dca2fb12fe912be634647b50
jatin98batra <jatin98batra@gmail.com>
2019-05-21 23:34:52 +0530

    Initioalize all the function pointers

---
+++ Initialize.c
@@ -0,0 +1,23 @@
+#include"headers.h"
+#include"declarations.h"
+int Initialize()
+
+{
+
+//Initializing function pointer
+	
+_crma=CreateMaster;
+_cal=CalBitLen;
+_opn=OpenFile;
+_uni=Unique;
+_comp2=compress2;
+_comp3=compress3;
+_comp4=compress4;
+_comp5=compress5;
+_comp6=compress6;
+_comp7=compress7;
+_srh=Search;
+
+
+
+}
